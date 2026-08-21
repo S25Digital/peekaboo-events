ff8306c84273bb0e58f0f4e51a192548a211757f
Simranjeet Singh <12469462+singhs020@users.noreply.github.com>
2026-08-21 21:08:47 +0530

    Add addMiddleware export to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 export { default as useAnalytics } from './useAnalytics';
-export { initAnalytics, identify, reset, track, flush } from './core';
+export { initAnalytics, identify, reset, track, flush, addMiddleware } from './core';
 export type {
   TrackedEvent,
   TrackInput,
   AnalyticsConfig,
   AnalyticsProperties,
+  Middleware,
 } from './core';
