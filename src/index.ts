a0aa93f3cd7cd6111220b20bb311b6f47be882e4
Simranjeet Singh <12469462+singhs020@users.noreply.github.com>
2026-08-21 20:20:18 +0530

    Add new types for analytics in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export { default as useAnalytics } from './useAnalytics';
 export { initAnalytics, identify, reset, track, flush } from './core';
-export type { EventPayload, AnalyticsConfig } from './core';
+export type {
+  TrackedEvent,
+  TrackInput,
+  AnalyticsConfig,
+  AnalyticsProperties,
+} from './core';
